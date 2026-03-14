88da2a8ac212a311685e3f378173c1fa26f04af1
Shital Patil <shitalpatil9236@gmail.com>
2026-03-14 21:39:36 +0530

    Fix db config for production

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,29 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "resume_analyzer",
+//   password: "postgres123",
+//   port: 5432
+// });
+
+// module.exports = pool;
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "resume_analyzer",
-  password: "postgres123",
-  port: 5432
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "resume_analyzer",
+        password: "postgres123",
+        port: 5432,
+      }
+);
 
 module.exports = pool;
