e9dac09b93d3638118e5b277bee348d1fbae4faa
Patil-Shital-9236 <shitalpatil9236@gmail.com>
2026-03-15 11:12:31 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,24 @@
 
+// const { Pool } = require("pg");
+
+// const pool = new Pool(
+//   process.env.DATABASE_URL
+//     ? {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: { rejectUnauthorized: false },
+//       }
+//     : {
+//         user: "postgres",
+//         host: "localhost",
+//         database: "resume_analyzer",
+//         password: "postgres123",
+//         port: 5432,
+//       }
+// );
+
+// module.exports = pool;
+
+
 const { Pool } = require("pg");
 
 const pool = new Pool(
@@ -8,10 +28,10 @@ const pool = new Pool(
         ssl: { rejectUnauthorized: false },
       }
     : {
-        user: "postgres",
-        host: "localhost",
-        database: "resume_analyzer",
-        password: "postgres123",
+        user: "resume_analyzer_db_v8o3_user",
+        host: "dpg-d6qjkbn5gffc73er1l5g-a.oregon-postgres.render.com",
+        database: "resume_analyzer_db_v8o3",
+        password: "g6rh10AsuPD4dYfw2ZRjFNFPWmiOSLlh",
         port: 5432,
       }
 );
